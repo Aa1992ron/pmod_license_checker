e12cb721a00f6eb0516af4c555e03fd0ad710522
A_A_ron <spaaron@umich.edu>
2020-08-23 19:20:47 -0400

    added license data to source files

--- global_definitions.py
+++ global_definitions.py
@@ -1,3 +1,25 @@
+# Perl Module License Checker -- a utility for examining the current perl 
+# library, with the purpose of finding any perl modules which are not explicitly 
+# licensed as 'free software.'  Such modules will be specified as such in the report
+# generated with this program. 
+
+# global_definitions.py
+
+#     Copyright (C) 2020  Aaron Patrick Spencer
+#     spaaron@umich.edu 
+
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, version 3 of the License.
+
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import os
 #GTK+ imports
 import gi
